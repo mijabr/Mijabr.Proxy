367cda6480b453298a7cdd43b482e61bb3ad9760
Brydie <Michaelb@attachesoftware.com>
2019-12-30 12:36:00 +1100

    initial

---
+++ lib/Mijabr.Proxy/Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ProxyKit;
+
+namespace Mijabr.Proxy
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddProxy();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                Console.WriteLine($"Proxy Request: {context.Request.Path}");
+                await next();
+            });
+
+            app.Map("/home", home =>
+            {
+                home.RunProxy(context => context
+  //                  .ForwardTo("http://localhost:5002/")
+                    .ForwardTo("http://home/home/")
+                    .AddXForwardedHeaders()
+                    .Send());
+            });
+
+//            app.RunProxy(context => context
+//                .ForwardTo("http://localhost:5000/")
+////                .ForwardTo("http://mijabr-home/")
+//                .AddXForwardedHeaders()
+//                .Send());
+        }
+    }
+}
